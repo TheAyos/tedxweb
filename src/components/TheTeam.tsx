cb64ed6916ad3abb40fd47cabfdb9fa5c62460bb
Ayoub AGOUZOUL <ayoub.agouzoul@polytechnique.edu>
2026-04-09 14:34:24 +0200

    fix mobile layout inconsistencies

--- src/components/TheTeam.tsx
+++ src/components/TheTeam.tsx
@@ -6,22 +6,24 @@ const About: React.FC = () => {
     return (
 
         <>
-            <Container as="main" mb={5} pt={12} bg="black" color="white" maxW="container.xl" textAlign={"justify"}
+            <Container as="main" mb={5} pt={12} bg="black" color="white" maxW="full" textAlign={"justify"}
                 mx="auto"
-                px={{ base: 8, md: "10vw" }}>
+                px={0}>
                 <VStack gap={20} align="stretch">
 
                 <VStack align="stretch" gap={10}>
                     <Text as={"h2"} id="theteam" fontSize={"4xl"} fontWeight={"medium"}>
                         Who are we?
                     </Text>
-                    <Flex flexDir={{ base: "column", md: "column" }} align="left">
+                    <Flex flexDir="column" align="center">
                         <Image src="/images/2026/everyone.jpg" width={{ base: "100%", sm: "100%" }}  height={"auto"} maxW={{sm: "800px"}}/>
 
-                        <Text as={"p"} fontSize={"md"} fontWeight={"medium"} mt={{ base: 10, md: 10 }} px={{ base: 0, md: 0 }} w={{ base: "100%", md: "90%", lg: "80%" }}>
-                            We are an organization consisting of 19 students of the Bachelor of Science program at École Polytechnique. 
-                            Putting together our experience of living all around the world — from the US, through France, Slovakia, the Ivory Coast, India, to Russia and Albania — we aim to create an event that addresses global issues from our local perspective of being students at the IP Paris campus.
-                        </Text>
+                        <Box w={{ base: "100%", md: "90%", lg: "80%" }} mx="auto">
+                            <Text as={"p"} fontSize={"md"} fontWeight={"medium"} mt={{ base: 10, md: 10 }}>
+                                We are an organization consisting of 19 students of the Bachelor of Science program at École Polytechnique.
+                                Putting together our experience of living all around the world — from the US, through France, Slovakia, the Ivory Coast, India, to Russia and Albania — we aim to create an event that addresses global issues from our local perspective of being students at the IP Paris campus.
+                            </Text>
+                        </Box>
                     </Flex>
                     <Box mt={3} mb={7}>
                             <HashLink
@@ -45,7 +47,7 @@ const About: React.FC = () => {
                     </Text>
     
             
-                    <Box w={{ base: "100%", md: "90%", lg: "80%" }}>
+                    <Box w={{ base: "100%", md: "90%", lg: "80%" }} mx="auto">
                         <Text as={"p"} fontSize={"md"} fontWeight={"medium"} mt={{ base: 10, md: 0 }} px={{ base: 0, md: 0 }}>
                             In the spirit of discovering and spreading ideas, TED has created a program called TEDx. TEDx is a program of local, self-organized events that bring people together to share a TED-like experience. Our event is called TEDxÉcolePolytechnique, where x = independently organized TED event. <br /><br />
                             At our TEDxÉcolePolytechnique event, TED Talks video and live speakers will combine to spark deep discussion and connection in a small group. The TED Conference provides general guidance for the TEDx program, but individual TEDx events, including ours, are self-organized.
@@ -58,7 +60,7 @@ const About: React.FC = () => {
                         About <span style={{ color: "var(--tedx-red)" }}>TED</span>
                     </Text>
 
-                    <Box w={{ base: "100%", md: "90%", lg: "80%" }}>
+                    <Box w={{ base: "100%", md: "90%", lg: "80%" }} mx="auto">
                         <Text as={"p"} fontSize={"md"} fontWeight={"medium"} mt={{ base: 10, md: 0 }} px={{ base: 0, md: 0 }}>
                             TED is a nonprofit, nonpartisan organization dedicated to discovering, debating and spreading ideas that spark conversation, deepen understanding and
                             drive meaningful change. Our organization is devoted to curiosity, reason, wonder and the pursuit of knowledge — without an agenda. We welcome people
@@ -67,7 +69,7 @@ const About: React.FC = () => {
                         </Text>
                     </Box>
 
-                    <Box w={{ base: "100%", md: "90%", lg: "80%" }}>
+                    <Box w={{ base: "100%", md: "90%", lg: "80%" }} mx="auto">
                         <Text as={"p"} fontSize={"md"} fontWeight={"medium"} mt={{ base: 5, md: 0 }} px={{ base: 0, md: 0 }}>
                             TED began in 1984 as a conference where Technology, Entertainment and Design converged, but today it spans a multitude of worldwide communities and
                             initiatives exploring everything from science and business to education, arts and global issues. <br /><br />
